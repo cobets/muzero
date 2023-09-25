95b1b9ebd5a203a09a5bfbef0f5ae8db0daa7615
cobets <cobets@gmail.com>
2023-09-25 21:26:44 +0300

    sample_game refactor to single numpy choice call

--- replay_buffer.py
+++ replay_buffer.py
@@ -18,15 +18,11 @@ class ReplayBuffer(object):
         self.buffer.append(game)
 
     def sample_batch(self, num_unroll_steps: int, td_steps: int):
-        games = [self.sample_game() for _ in range(self.batch_size)]
+        games = numpy.random.choice(self.buffer, self.batch_size)
         game_pos = [(g, self.sample_position(g)) for g in games]
         return [(g.make_image(i), g.history[i:i + num_unroll_steps],
                  g.make_target(i, num_unroll_steps, td_steps, g.to_play()))
                 for (g, i) in game_pos]
-
-    def sample_game(self) -> Game:
-        # Sample game from buffer either uniformly or according to some priority.
-        return numpy.random.choice(self.buffer)
 
     def sample_position(self, game) -> int:
         # Sample position from game either uniformly or according to some priority.
